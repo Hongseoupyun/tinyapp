b3b55a15a088d8b0c514e502e5687b61398a17b0
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2022-04-19 00:36:15 -0400

    A  POST Route to Receive the Form Submission added

--- express_server.js
+++ express_server.js
@@ -2,7 +2,9 @@ const express = require("express");
 const { all } = require("express/lib/application");
 const app = express();
 const PORT = 8080;
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -37,6 +39,10 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars );
 }); 
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
